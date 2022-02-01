176c6d737cdc7a79acf56f24c4b562e60f30e386
SHUBHAM MORE <moreshubham1251p601@gmail.com>
2022-02-01 21:23:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -52,50 +52,6 @@ function App (){
 }
 
 
-// export default class App extends Component {
-  
-//   pageSize = 6;
-//   apiKey = process.env.REACT_APP_NEWS_API
-
-//   state = {
-//     progress: 0
-//   }
-
-//   setProgress = (progress) =>{
-//     setState({progress: progress})
-//   }
-//   render() {
-//     return (
-//       <div>
-//         <Router>
-//           <Navbar />
-//           <LoadingBar color='#0000FF' height={4} progress={progress} />
-//         <Routes>
-//           <Route exact path="/" element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country='in' category="general"/>} />
-
-//           <Route exact path="/business" element={<News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country='in' category="business"/>} />
-
-//           <Route exact path="/entertainment" element={<News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country='in' category="entertainment"/>} />
-
-//           <Route exact path="/health" element={<News setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country='in' category="health"/>} />
-
-//           <Route exact path="/science" element={<News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country='in' category="science"/>} />
-
-//           <Route exact path="/sports" element={<News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country='in' category="sports"/>} /> 
-
-//           <Route exact path="/technology" element={<News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country='in' category="technology"/>} />
-
-//           <Route exact path="/general" element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country='in' category="general"/>} />
-
-//         </Routes>
-//         </Router>
-//       </div>
-//     )
-//   }
-// }
-
-
-
 
 export default App;
 
