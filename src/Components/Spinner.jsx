e7f6a93ae8b52ba1b60380b1bbc1a2201d184f5f
SHUBHAM MORE <moreshubham1251p601@gmail.com>
2022-02-01 21:27:28 +0530

    Update Spinner.jsx

--- src/Components/Spinner.jsx
+++ src/Components/Spinner.jsx
@@ -11,16 +11,5 @@ const Spinner = () =>{
     )
 }
 
-// export class Spinner extends Component {
-//     render() {
-//         return (
-//             <div className="d-flex justify-content-center">
-//                 <div className="spinner-border text-success" role="status">
-//                     {/* <span className="visually-hidden">Loading...</span> */}
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
 
 export default Spinner
